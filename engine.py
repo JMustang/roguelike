00b1c95d8361fff8c6d5efdff210d0344c927d9a
Junior <eecfredes@gmail.com>
2024-11-14 22:35:01 -0300

    :rocket: Adding game map to engine

--- engine.py
+++ engine.py
@@ -5,33 +5,40 @@ from tcod.console import Console
 
 from actions import EscapeAction, MovementAction
 from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
 
 
 class Engine:
     def __init__(
-        self, entities: Set[Entity], event_handler: EventHandler, player: Entity
+        self,
+        entities: Set[Entity],
+        event_handler: EventHandler,
+        game_map: GameMap,
+        player: Entity,
     ):
         self.entities = entities
         self.event_handler = event_handler
+        self.game_map = game_map
         self.player = player
 
-    def handler_events(self, events: Iterable[Any]) -> None:
+    def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
             action = self.event_handler.dispatch(event)
 
             if action is None:
                 continue
 
-            if isinstance(action, MovementAction):
-                self.player.move(dx=action.dx, dy=action.dy)
-            elif isinstance(action, EscapeAction):
-                raise SystemExit()
+            action.perform(self, self.player)
 
     def render(self, console: Console, context: Context) -> None:
+        console.clear()
+
+        # Desenhar o mapa primeiro
+        self.game_map.render(console)
+
+        # Depois desenhar todas as entidades
         for entity in self.entities:
             console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
-
-        console.clear()
