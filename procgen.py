06b17414c7c1f490778a0a88c87a18a5ffb0ea44
Junior <eecfredes@gmail.com>
2024-11-16 19:11:10 -0300

    :rocket: Created a tunnel between dungeons

--- procgen.py
+++ procgen.py
@@ -1,4 +1,8 @@
-from typing import Tuple
+import random
+
+from typing import Iterator, Tuple
+
+import tcod
 
 from game_map import GameMap
 import tile_types
@@ -22,6 +26,26 @@ class RectangularRoom:
     def inner(self) -> Tuple[int, int]:
         """Return the inner area of this room as a 2D array index."""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+
+
+def tunnel_between(
+    start: Tuple[int, int], end: Tuple[int, int]
+) -> Iterator[Tuple[int, int]]:
+    """Return an L-shaped tunnel between these two points."""
+    x1, y1 = start
+    x2, y2 = end
+    if random.random() < 0.5:  # 50% chance.
+        # Move horizontally, then vertically.
+        corner_x, corner_y = x2, y1
+    else:
+        # Move vertically, then horizontally.
+        corner_x, corner_y = x1, y2
+
+    # Generate the coordinates for this tunnel.
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y
 
 
 def generate_dungeon(map_width, map_height) -> GameMap:
