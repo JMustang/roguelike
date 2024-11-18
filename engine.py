42ad2f787c84d2663440bb63a0efe81e88ad8b55
Junior <eecfredes@gmail.com>
2024-11-18 20:59:26 -0300

    change the engine to auto generate the enemy

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-from typing import Set, Iterable, Any
+from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
@@ -12,12 +12,10 @@ from input_handlers import EventHandler
 class Engine:
     def __init__(
         self,
-        entities: Set[Entity],
         event_handler: EventHandler,
         game_map: GameMap,
         player: Entity,
     ):
-        self.entities = entities
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
@@ -49,12 +47,5 @@ class Engine:
 
         # Desenhar o mapa primeiro
         self.game_map.render(console)
-
-        # Depois desenhar todas as entidades
-        for entity in self.entities:
-            if self.game_map.visible[entity.x, entity.y]:
-                console.print(
-                    x=entity.x, y=entity.y, string=entity.char, fg=entity.color
-                )
 
         context.present(console)
