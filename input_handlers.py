4c077d1a1ccc0177ef2a66d12a75225ecdcde9b8
Junior <eecfredes@gmail.com>
2024-11-23 18:49:37 -0300

    :rocket: init and handle events function, created

--- input_handlers.py
+++ input_handlers.py
@@ -1,11 +1,32 @@
-from typing import Optional
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
 from actions import Action, BumpAction, EscapeAction
 
 
+if TYPE_CHECKING:
+    from engine import Engine
+
+
 class EventHandler(tcod.event.EventDispatch[Action]):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
+    def handle_events(self) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch(event)
+
+            if action is None:
+                continue
+
+            action.perform()
+
+            self.engine.handle_enemy_turns()
+            self.engine.update_fov()
+
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
 
