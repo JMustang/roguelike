98893aa0d4481bfdfeec9478a20ebbe777e0b6cb
Junior <eecfredes@gmail.com>
2024-12-02 21:35:15 -0300

    :rocket: Function actors, created

--- game_map.py
+++ game_map.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from typing import Iterable, Optional, TYPE_CHECKING
+from typing import Iterable, Iterator, Optional, TYPE_CHECKING
 
 import numpy as np
 from tcod.console import Console
 
+from entity import Actor
 import tile_types
 
 if TYPE_CHECKING:
@@ -28,6 +29,15 @@ class GameMap:
         self.explored = np.full(
             (width, height), fill_value=False, order="F"
         )  # Tiles the player has seen before
+
+    @property
+    def actors(self) -> Iterator[Actor]:
+        """Iterate over this maps living actors."""
+        yield from (
+            entity
+            for entity in self.entities
+            if isinstance(entity, Actor) and entity.is_alive
+        )
 
     def get_blocking_entity_at_location(
         self, location_x: int, location_y: int
