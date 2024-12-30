469f5d6ab6af6ccd00d189e0a8d903e3222c81bf
Junior <eecfredes@gmail.com>
2024-12-30 20:37:54 -0300

    :rocket: Down stair location

--- game_map.py
+++ game_map.py
@@ -30,6 +30,8 @@ class GameMap:
             (width, height), fill_value=False, order="F"
         )  # Tiles the player has seen before
 
+        self.downstairs_location = (0, 0)
+
     @property
     def gamemap(self) -> GameMap:
         return self
