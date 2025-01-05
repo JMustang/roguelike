b5f1d3112e9b4c9f63e98a7980ec7c29aa5ca52b
Junior <eecfredes@gmail.com>
2025-01-05 14:30:17 -0300

    :rocket: Equip Action class, created

--- actions.py
+++ actions.py
@@ -80,6 +80,16 @@ class DropItem(ItemAction):
         self.entity.inventory.drop(self.item)
 
 
+class EquipAction(Action):
+    def __init__(self, entity: Actor, item: Item):
+        super().__init__(entity)
+
+        self.item = item
+
+    def perform(self) -> None:
+        self.entity.equipment.toggle_equip(self.item)
+
+
 class WaitAction(Action):
     def perform(self) -> None:
         pass
